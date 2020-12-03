c7c6c5e0fb006c36bebd58b9c5334ceaa06924a9
Javier De La Fuente <javierdelafuentejr@gmail.com>
2020-12-02 21:23:23 -0600

    Complete connecting Video List and Player components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,23 +3,44 @@ import VideoPlayer from './VideoPlayer.js';
 import Search from './Search.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><Search /></div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videoList: exampleVideoData,
+      currentVideo: exampleVideoData[0]
+    };
+
+    this.clickedVideo = this.clickedVideo.bind(this);
+  }
+
+  clickedVideo(click) {
+    this.setState({
+      currentVideo: click
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><Search /></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><VideoPlayer video={this.state.currentVideo}/></div>
+          </div>
+          <div className="col-md-5">
+            <div><VideoList clicker={this.clickedVideo} videos={this.state.videoList} /></div>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><VideoPlayer video={exampleVideoData[0]}/></div>
-      </div>
-      <div className="col-md-5">
-        <div><VideoList videos={exampleVideoData} /></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
